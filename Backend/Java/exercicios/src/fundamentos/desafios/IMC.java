d99c2532f1495566ead9f383c91deb8a483956af
Vinícius Magalhães Batista <v.magalhaesb@hotmail.com>
2026-01-21 00:13:27 -0300

    calculadora de IMC

---
+++ Backend/Java/exercicios/src/fundamentos/desafios/IMC.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class IMC {
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Caculadora de IMC");
+
+        System.out.println("Digite o seu peso:");
+
+        double peso = entrada.nextDouble();
+
+        System.out.println("Digite a sua altura:");
+
+        double altura = entrada. nextDouble();
+
+        double imc = peso/(altura*altura);
+
+        System.out.printf("Seu IMC e de: %.2f",imc);
+    }
+
+
+}
