e557a3cd338272039ce6f6b65b1e65f88b2dc50f
Vinícius Magalhães Batista <v.magalhaesb@hotmail.com>
2026-02-24 00:16:07 -0300

    aula for e while

--- Backend/Java/exercicios/src/controle/DesafioDiaSemana.java
+++ Backend/Java/exercicios/src/controle/DesafioDiaSemana.java
@@ -9,24 +9,22 @@ public class DesafioDiaSemana {
         Scanner entrada = new Scanner(System.in);
         String semana = entrada.next();
         
-        if(semana.equals("domingo")){
+        if(semana.equalsIgnoreCase("domingo")){
             System.out.println(1);
-        }else if(semana.equals("segunda")){
+        }else if(semana.equalsIgnoreCase("segunda")){
             System.out.println(2);
-        }else if(semana.equals("terça")){
+        }else if(semana.equalsIgnoreCase("terça")){
             System.out.println(3);
-        }else if(semana.equals("quarta")){
+        }else if(semana.equalsIgnoreCase("quarta")){
             System.out.println(4);
-        }else if(semana.equals("quinta")){
+        }else if(semana.equalsIgnoreCase("quinta")){
             System.out.println(5);
-        }else if(semana.equals("sexta")){
+        }else if(semana.equalsIgnoreCase("sexta")){
             System.out.println(6);
-        }else if(semana.equals("sabado")){
+        }else if(semana.equalsIgnoreCase("sabado")){
             System.out.println(7);
         }else{
             System.out.println("ERRO");
         }
-        
     }
-
 }
