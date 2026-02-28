0cde5d650f043ba7b2eee30e39c28da76cc9b645
Vinícius Magalhães Batista <v.magalhaesb@hotmail.com>
2026-02-28 01:57:42 -0300

    DesafioWhile

---
+++ Backend/Java/exercicios/src/controle/DesafioWhile.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class DesafioWhile {
+    public static void main(String[] args) {
+
+        System.out.println("Digite as notas dos alunos");
+
+        Scanner entrada = new Scanner(System.in);
+        double nota = 0;
+        double total = 0;
+        int cont = 0;
+
+        while(nota != -1){
+            nota = entrada.nextDouble();
+
+            if(nota <= 10 && nota >= 0){
+                total += nota;
+                cont++;
+            }
+        }
+   
+
+        double media = total/cont;
+
+        System.out.printf("O total das notas de alunos e de %s de %s alunos,"
+        +" com isso a media e de %s",total,cont, media);
+
+    }  
+}
