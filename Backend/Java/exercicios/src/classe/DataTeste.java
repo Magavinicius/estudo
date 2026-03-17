8f3e8c46f80e0d1aeaabca875c40d312a33858b1
Vinícius Magalhães Batista <v.magalhaesb@hotmail.com>
2026-03-17 00:47:10 -0300

    add desafio data

---
+++ Backend/Java/exercicios/src/classe/DataTeste.java
@@ -0,0 +1,16 @@
+public class DataTeste {
+    public static void main(String[] args) {
+        
+        Data data1 = new Data();
+        data1.dia = 17;
+        data1.mes = 03;
+        data1.ano = 2026;
+
+        Data data2 = new Data();
+        data2.dia = 24;
+        data2.mes = 02;
+        data2.ano = 1998;
+
+    }
+
+}
